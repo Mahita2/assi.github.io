d5ee5a277576114c5a02448fe50a03eeb2efc983
Mahita2 <54814394+Mahita2@users.noreply.github.com>
2020-07-06 16:50:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+(function(){
+
+angular.module('lunchChecker',[])
+.controller('lunchController', lunchController);
+
+lunchController.$inject = ['$scope'];
+
+function lunchController($scope){
+
+	$scope.list = "" ;
+
+	$scope.check = function (){
+
+		if($scope.list.trim() != ""){
+			$scope.textStyle = {"border":"1px solid green"};
+			$scope.messageStyle = {"color":"green"};
+			if($scope.list.split(",").length < 4){
+				$scope.message = "Enjoy!"
+			}else{
+				$scope.message = "Too much!" 
+			}
+		}else{
+			$scope.textStyle = {"border":"1px solid red"};
+			$scope.messageStyle = {"color":"red"};
+			$scope.message = "Please enter data first"
+		}
+	};
+};
+
+})();
+
